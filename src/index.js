b186c6c7718825dab6e3235045c8b5c346acd3a5
Maria Zelenova <zma611@yandex.ru>
2023-03-27 19:20:51 +0300

    change BrouserRoute on HashRouter

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      {/* <Routes>
-        <Route path="/" element={<App />} />
-      </Routes> */}
+    {/* <BrowserRouter> */}
+    <HashRouter> 
       <App/>
-    </BrowserRouter>
+      </HashRouter>
+     
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
