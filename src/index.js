6e085e79c7df4dc4333c7e8aefb51876c4489082
Maria Zelenova <zma611@yandex.ru>
2023-03-27 19:22:46 +0300

    fix router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
